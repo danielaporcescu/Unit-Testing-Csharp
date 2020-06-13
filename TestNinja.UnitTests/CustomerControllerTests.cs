c8f7219ed7082b0fd30c45e8c72abe7eb951d864
Daniela Porcescu <dporcescu@grundfos.com>
2020-06-13 11:21:53 +0200

    testing the return type of methods

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+
+            //Not Found
+            Assert.That(result, Is.TypeOf<NotFound>());
+            
+            //NotFoundObject or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+
+        }
+    }
+}
