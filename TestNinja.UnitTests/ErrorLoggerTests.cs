0a8695af5806e1a81aa3e92b25aff44cb12f15a3
Daniela Porcescu <dporcescu@grundfos.com>
2020-06-13 11:41:21 +0200

    testing methods that raise an event

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -33,5 +33,18 @@ namespace TestNinja.UnitTests
             //Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>);
 
         }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLogEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
     }
 }
