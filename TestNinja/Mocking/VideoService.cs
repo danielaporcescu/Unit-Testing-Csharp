c3e096311dfc97b6cf707bb4e3db3a623992b49e
Daniela Porcescu <dporcescu@grundfos.com>
2020-06-17 10:19:03 +0200

    refactoring for testing

--- TestNinja/Mocking/VideoService.cs
+++ TestNinja/Mocking/VideoService.cs
@@ -10,10 +10,12 @@ namespace TestNinja.Mocking
     public class VideoService
     {
         private IFileReader _fileReader;
+        private IVideoRepository _repository;
 
-        public VideoService(IFileReader fileReader = null)
+        public VideoService(IFileReader fileReader = null, IVideoRepository repository=null)
         {
             _fileReader = fileReader ?? new FileReader();
+            _repository = repository ?? new VideoRepository();
         }
 
         public string ReadVideoTitle()
@@ -28,19 +30,13 @@ namespace TestNinja.Mocking
         public string GetUnprocessedVideosAsCsv()
         {
             var videoIds = new List<int>();
+
+            var videos = _repository.GetUnprocessedVideos();
             
-            using (var context = new VideoContext())
-            {
-                var videos = 
-                    (from video in context.Videos
-                    where !video.IsProcessed
-                    select video).ToList();
-                
                 foreach (var v in videos)
                     videoIds.Add(v.Id);
 
                 return String.Join(",", videoIds);
-            }
         }
     }
 
