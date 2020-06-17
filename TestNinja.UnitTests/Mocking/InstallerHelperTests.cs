422316921b2da0ed49a59e08e06cbd2420c45fb0
Daniela Porcescu <dporcescu@grundfos.com>
2020-06-17 12:15:41 +0200

    testing intallerHelper

--- TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
+++ TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -1,19 +1,47 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
     [TestFixture]
     class InstallerHelperTests
     {
-        [Test]
-        public void DownloadInstaller_()
-        {
+        private Mock<IFileDownloader> _fileDownloader;
+        private InstallerHelper _installerHelper;
 
+        [SetUp]
+        public void SetUp()
+        {
+            _fileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_fileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFails_ReturnFalse()
+        {
+            _fileDownloader.Setup(fd => 
+            fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>()))
+                .Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.False);
+        }
+
+
+        [Test]
+        public void DownloadInstaller_DownloadSuccess_ReturnTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.True);
         }
     }
 }
